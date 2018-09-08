6d39edc9bd7b29a10ed06ea50acb445f91c7675b
Shivam Thapliyal <thapliyalshivam@gmail.com>
2018-09-09 01:50:25 +0530

    Path

--- app/src/main/java/com/example/tshivam/customview/WaterView.java
+++ app/src/main/java/com/example/tshivam/customview/WaterView.java
@@ -3,6 +3,7 @@ package com.example.tshivam.customview;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -10,6 +11,7 @@ import android.view.View;
 public class WaterView extends View {
 
     Paint ctx;
+    int a = 0;
 
     public WaterView(Context context) {
         super(context);
@@ -32,6 +34,29 @@ public class WaterView extends View {
     }
 
 
+    public Path createPath(int sides, float radius ) {
+        int cx=10;
+        int cy = 10;
+        Path path = new Path();
+        double angle = 2.0 * Math.PI / sides;
+        path.moveTo(
+                cx + (float)(radius * Math.cos(0.0)),
+                cy + (float)(radius * Math.sin(0.0)));
+        for (int i=1;i<=sides;++i ) {
+            path.lineTo(
+                    cx + (float)(radius * Math.cos(angle * i)),
+                    cy + (float)(radius * Math.sin(angle * i)));
+        }
+        path.close();
+        return path;
+    }
+
+
+    public void update(){
+        a= a>10?0:a+1;
+        postInvalidate();
+    }
+
     public void init(){
          ctx = new Paint();
         ctx.setStrokeWidth(2);
@@ -41,7 +66,9 @@ public class WaterView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
        // super.onDraw(canvas);
-        canvas.drawLine(0,0,10,10,ctx);
+        update();
+        canvas.drawLine(a,0,a,200,ctx);
+        canvas.drawPath(createPath(5,20),ctx);
 
     }
 }
